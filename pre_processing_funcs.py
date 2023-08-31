a70ae526cbfe185bb505c99d01d47e992b28fdc4
Ashraf Abdulkhaliq <ashrafabdulkhaliq80@gmail.com>
2023-08-31 18:23:37 +0300

    update notebooks

---
+++ pre_processing_funcs.py
@@ -0,0 +1,191 @@
+import pandas as pd
+
+def set_index(df):
+    df = (
+        df
+        .rename(columns={"Unnamed: 0": "id"})
+        .set_index("id")
+    )
+    return df
+
+
+
+def split_person_description(df):
+    
+    df[["personal", "deg_work"]] = (
+        df["Person Description"]
+        .str
+        .split(", education: ", expand=True)
+    )
+    
+    df[["Marriage", "Gender", "with", "Children", "tc"]] = (
+        df["personal"]
+        .str
+        .split(expand=True)
+    )
+    
+    df[["Degree", "Work"]] = (
+        df["deg_work"]
+        .str
+        .split("working as", expand=True)
+    )
+    
+    df = df.drop(columns=["Person Description", "personal", "with", "tc", "deg_work"])
+    return df
+
+
+def split_customer_order(df):
+    df[["ord_dep", "Oreder Brand"]] = (
+        df["Customer Order"]
+        .str
+        .split(", Ordered Brand : ", expand=True)
+    )
+    
+    df[["Product", "Department", "blank"]] = (
+        df["ord_dep"]
+        .str
+        .split("from | department", expand=True)
+    )
+        
+    df = df.drop(columns=["Customer Order", "ord_dep", "blank"])
+    return df
+
+
+def encode_market_features(df):
+    unique_feat = set()
+    
+    for feat_list in df["Additional Features in market"] :
+        if pd.notna(feat_list):
+            string_data = feat_list.strip("[]")
+            elements = string_data.split(', ')
+            elements = [element.strip("'") for element in elements]
+            unique_feat.update(elements)
+
+    for feat in unique_feat:
+        df[feat] = (
+            df["Additional Features in market"]
+            .apply(lambda x: 1 if pd.notna(x) and feat in x else 0)
+        )
+        
+    df = df.drop(columns="Additional Features in market")
+    return df
+
+
+
+def transform_cost_sales(df):
+    df["Store Sales"] = df["Store Sales"].str.split(expand=True)[0].astype(float) * 1e6
+    df["Store Cost"] = df["Store Cost"].str.split(expand=True)[0].astype(float) * 1e6
+    return df
+
+
+
+def extract_product_weights(df):
+    if "Product Weights Data in (KG)" in df.columns:
+        df[["b1", "Gross Weight", "Net Weight", "Package Weight", "b2"]] = (
+            df["Product Weights Data in (KG)"]
+            .str
+            .split("{'Gross Weight': |, 'Net Weight': |, 'Package Weight': |}", expand=True)
+        )
+        df = df.drop(columns=["b1", "b2", "Product Weights Data in (KG)"])
+    elif "Weights Data" in df.columns:
+        df[["b1", "Gross Weight", "Net Weight", "Package Weight", "b2"]] = (
+            df["Weights Data"]
+            .str
+            .split("{'Gross Weight': |, 'Net Weight': |, 'Package Weight': |}", expand=True)
+        )
+        df = df.drop(columns=["b1", "b2", "Weights Data"])
+    return df
+
+
+
+def transform_recyclable(df):
+    mapping = {'recyclable': 'yes', 'non recyclable': 'no'}
+    df["Is Recyclable?"] = df["Is Recyclable?"].map(mapping)
+    return df
+
+
+
+def transform_income(df):
+    if "Min. Yearly Income" in df.columns:
+        df["Min. Person Yearly Income"] = (
+            df["Min. Yearly Income"]
+            .str
+            .split("K+", expand=True)[0]
+            .astype(float) * 1000
+        )
+        df = df.drop(columns="Min. Yearly Income")
+    elif "Min. Person Yearly Income" in df.columns:
+        df["Min. Person Yearly Income"] = (
+            df["Min. Person Yearly Income"]
+            .str
+            .split("K+", expand=True)[0]
+            .astype(float) * 1000
+        )
+    elif "Yearly Income" in df.columns:
+        df["Min. Person Yearly Income"] = (
+            df["Yearly Income"]
+            .str
+            .split("K+", expand=True)[0]
+            .astype(float) * 1000
+        )
+        df = df.drop(columns="Yearly Income")
+    return df
+
+
+def transform_columns_type(df):
+    df["Store Area"] = (
+        df["Store Area"]
+        .replace('missing', float('nan'))
+        .astype(float)
+    )
+    
+    df["Grocery Area"] = (
+        df["Grocery Area"]
+        .str
+        .strip('"')
+        .replace('missing', float('nan'))
+        .astype(float)
+    )
+    
+    df["Meat Area"] = (
+        df["Meat Area"]
+        .str
+        .strip('"')
+        .astype(float)
+    )
+    
+    trans = ["Gross Weight", "Net Weight", "Package Weight"]
+    
+    df[trans] = df[trans].astype(float)
+    
+    return df
+
+
+def calculate_package_weight(df):
+    df["Package Weight"] = df["Gross Weight"] - df["Net Weight"]
+    return df
+
+
+def fill_nulls(df):
+    number_cols = df.select_dtypes([float, int]).columns
+    cat_cols = df.select_dtypes("object").columns
+    
+    if "Cost" in df.columns:
+        df[number_cols] = (
+            df[number_cols]
+            .fillna(df[number_cols].mean())
+        )
+        
+    else:
+        df[number_cols] = (
+            df[number_cols]
+            .fillna(df[number_cols].mean())
+        )
+        
+        for col in cat_cols:
+            df[col] = (
+                df[col]
+                .fillna(df[col].mode()[0])
+            )
+
+    return df
